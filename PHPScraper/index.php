3f548a2d2b6401992891c1f2e603fa2562ef583d
Kenny Lozowski <kennylozowski@gmail.com>
2016-06-17 15:19:16 -0600

    add the scraper, here it is.

--- PHPScraper/index.php
+++ PHPScraper/index.php
@@ -1,27 +1,21 @@
 <?php
-
-print "54.0";
 /**
-$curlobj = curl_init();
-$now = time();
-$fullurl = "http://tvc4.forexpros.com/8f1b249be4bfff946c9c922334154c5e/1465325428/68/68/8/history?symbol=8849&resolution=15&from=1465319440&to=1465325440";
-curl_setopt($curlobj, CURLOPT_URL, $fullurl);
-curl_setopt($curlobj, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curlobj, CURLOPT_TIMEOUT, '8');
-curl_setopt($curlobj, CURLOPT_HTTPHEADER, array(
-   'Accept-Encoding: gzip, deflate, sdch', 
-	 'Accept-Language: en-US,en;q=0.8', 
-   'Upgrade-Insecure-Requests: 1' ,
-	 'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36' ,
-	 'Cache-Control: max-age=0',
- 	 'Connection: keep-alive' ,
-    ));
-    
-  
-$curlstr = curl_exec($curlobj);
-print_r(curl_getinfo($curlobj));  
-print "hi";
-print ($curlstr);
-curl_close($curlobj);
+Oil Price Scraper
+Kenny Lozowski - kenny_lozowski@banffcentre.ca
+for Maria Michails
 
+Goes to Yahoo and gets oil price -- Yahoo updates once a minute with a 30 minute latency against the market
+Uses the CURL library, a simple HTTP client
 */
+
+$curlobj = curl_init(); // initialize the CURL library
+$dom = new DOMDocument(); //initializes the XML DOM parser
+$fullurl = "https://finance.yahoo.com/q?s=CLQ16.NYM"; //this is the URL to get at yahoo, with the NYMEX crude oil symbol as a query argument
+$oilprice_elementid = "yfs_l10_clq16.nym"; //the ID of the DOM element which contains the crude oil price -- **note this may change and break the scraper**
+curl_setopt($curlobj, CURLOPT_URL, $fullurl); // add the URL argument to curl
+curl_setopt($curlobj, CURLOPT_RETURNTRANSFER, true); //curl should store the returned HTTP result in a long string, not print it
+$curlstr = curl_exec($curlobj); //run CURL
+curl_close($curlobj); // clean up CURL
+@$dom->loadHtml($curlstr); //load the CURL string into the DOM parser
+$oilprice = $dom->getElementByID($oilprice_elementid)->textContent; //get the text content of the element enclosing the oil price 
+print "~".$oilprice."~"; // print it
